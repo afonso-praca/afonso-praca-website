d44c46fa04e7c2ccbefc64a08de8da3a1c6d4b72
afonso-praca <afonsoinfo@gmail.com>
2013-02-14 20:47:15 -0200

    project added

---
+++ js/libs/script.js
@@ -0,0 +1,64 @@
+(function(){
+
+	// on document ready
+	$(document).ready(function(){
+		init();
+	});
+
+	function init () {
+		loadImg();
+	}
+
+	function loadImg(){
+		var img = new Image();
+
+        img.onload = function() {
+			animaInit();
+        }
+
+        img.onerror = function() {
+            console.log("erro no carregamento da imagem");
+            animaInit();
+        }
+
+        img.src = "http://afonsopraca.com.br/img/birds2.jpg";
+	}
+
+	function animaInit(){
+		posImg();
+		addListeners();
+		$('#wrapper').fadeTo(600, 1, function(){
+			$('#content').delay(200).fadeTo(600, 1);
+		});
+	}
+
+	//set size of bg image
+	function posImg(){
+		var $img_bg = $('#bg img');
+		var a = ($(window).width() / $img_bg.width());
+		var b = ($(window).height()/ $img_bg.height());
+		var c = Math.max(a, b);
+
+		$img_bg.width($img_bg.width()*c);
+		$img_bg.height($img_bg.height()*c);
+	}
+
+	//add HTMLListeners
+	function addListeners(){
+	    $(window).resize(function(){
+	        posImg();
+	    });
+
+	    $('#projetos li, #contatos a')
+		    .on("mouseover", function(){
+		    	$(this).siblings().stop().animate({ opacity: '0.6' }, 300);
+		    })
+		    .on("mouseout", function(){
+		    	$(this).siblings().stop().animate({ opacity: '1' }, 400);
+			});
+	}
+
+})();
+
+
+
